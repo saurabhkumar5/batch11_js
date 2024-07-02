cb806a96fb0643919d3f9e3d4be169ad5690a013
saurabh kumar <saurabhkumar9493@gmail.com>
2024-07-02 11:55:59 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+
+
+//    console.log(10)
+//    console.log(20)
+//    console.log("sohan")
+
+
+    //   a = 10
+    //   a = 50
+    //   b = 20
+    //   b = 10
+    //   console.log(a*b)
+               
+            // ''
+            // ""
+
+            // var a = 10;
+            // var b = 20;
+            // var c = "aman"
+            // // console.log(a)
+            // // console.log(b)
+            // // console.log(c)
+
+            // console.log(a,b,c)
+
+
+
+
+            // var _a = 10
+            // console.log(_a)
+
+
+
+            // var a = 10
+            // var b = 20
+            // console.log(a+b)
+            // console.log(a-b)
+            // console.log(a*b)
+            // console.log(a/b)
+
+
+            //   var a = 10; 
+            //   var b = 20; 
+            //   var c = 30;
+            //   console.log(a+b+c);
+            //   console.log(100+200+3000+40)
+             
+            //    let a = 20;
+            //    let b = 30;
+            //    console.log(a,b)
+              
+
+            const a = 10;
+            const b = 20;
+            console.log(a,b)
+            
